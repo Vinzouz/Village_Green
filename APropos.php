853ba683187229b429b0b49fed2a07506d5d19d0
FastRives <57791635+FastRives@users.noreply.github.com>
2019-11-15 09:45:56 +0100

    Add files via upload

---
+++ APropos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.1.0/css/flag-icon.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+  <link rel="stylesheet" href="Style.css" />
+  <link rel="stylesheet" href="APropos.css">
+  
+
+  <title>Village Green - A Propos</title>
+</head>
+
+<body>
+<?php include("Ext_header.php");?>
+
+<section id='contap' class="container">
+
+<div class="row col-12"><h2 id="titre">A Propos : </h2></div>
+
+</section>
+
+<?php include("Ext_footer.php");?>
+<?php include("Ext_script.php");?>
