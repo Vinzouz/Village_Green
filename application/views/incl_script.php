1aedf615b2b058230236ac98e81b580f87718a76
FastRives <57791635+FastRives@users.noreply.github.com>
2020-01-09 16:31:13 +0100

    17eme commit
    Modification pour le loader du fichier incl_script.

--- application/views/incl_script.php
+++ application/views/incl_script.php
@@ -4,3 +4,13 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.5/jquery.bxslider.js"></script>
 <script src="https://kit.fontawesome.com/2af6969b3e.js" crossorigin="anonymous"></script>
 <script src=<?= base_url('assets/js/Script.js') ?>></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+<script type="text/javascript">
+
+window.onload = function() {
+    $(".loader").hide();
+    $(".allPage").fadeIn(500);
+}
+</script>
+
+
