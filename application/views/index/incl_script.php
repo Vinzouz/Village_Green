1a9da70d3fa1e21b57269c976dff4792cf9d0725
Vinzouz <57451392+Vinzouz@users.noreply.github.com>
2020-01-30 15:37:10 +0100

    Delete incl_script.php

--- application/views/index/incl_script.php
+++
@@ -1,6 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.5/jquery.bxslider.js"></script>
-<script src="https://kit.fontawesome.com/2af6969b3e.js" crossorigin="anonymous"></script>
-<script src=<?= base_url('assets/js/Script.js') ?>></script>
