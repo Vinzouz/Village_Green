387eafba86dadb62ca3e5b3a6fe5def634fbd1f4
unknown <vincent.andre1893@gmail.com>
2020-02-23 18:36:55 +0100

    Commit Vinz
    searchbar et navbar admin

--- application/models/Model_search.php
+++ application/models/Model_search.php
@@ -8,6 +8,7 @@ class Model_search extends CI_Model{
         $recherche = $this->input->post('search_value');
         //$this->db->select('produit_nom');
         $this->db->like('produit_nom', $recherche)->limit(10);
+        $this->db->or_like('produit_marque', $recherche);
         
         $select = $this->db->get('produit');
 
