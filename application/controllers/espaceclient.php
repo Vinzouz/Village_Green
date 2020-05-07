5e6377708345077022c4dc43a4eafe6eb56c801f
unknown <vincent.andre1893@gmail.com>
2020-05-07 18:44:13 +0200

    Commit Vinz
    Commentaires controlleur espace client et modèle

--- application/controllers/espaceclient.php
+++ application/controllers/espaceclient.php
@@ -1,42 +1,44 @@
 <?php
 
-class espaceclient extends CI_Controller {
+class espaceclient extends CI_Controller
+{
 
-    public function __construct(){
-
+    public function __construct() // Fonction constructrice qui charge les modèles pour tout le controller
+    {
         parent::__construct();
         $this->load->model('Model_espaceclient');
         $this->load->model('Model_home');
     }
 
-public function espaceC()
-        {
-        
-                if ($this->session->userdata('user_id') == null) {
-                    redirect('');
-                }else{
-                $array['data'] = $this->Model_espaceclient->getEspaceC();
-                $array['dataC'] = $this->Model_espaceclient->getClient();
-                $navbarRub['getRubriques'] = $this->Model_home->getRubriques();
-                $this->load->view('include/incl_head');
-                $this->load->view('include/incl_navbar', $navbarRub);
-                $this->load->view('incl_user_area', $array);
-                $this->load->view('include/incl_footer');
-                $this->load->view('include/incl_script');
-                }
+    public function espaceC() // Fonction espaceC qui charge l'espace client de l'utilisateur 
+    { // Affichage des commandes dans l'espace client
+
+        if ($this->session->userdata('user_id') == null) { // Si l'id de l'utilisateur est incorrect ou n'existe pas redirection page d'accueil
+            redirect('');
+        } else {
+            $array['data'] = $this->Model_espaceclient->getEspaceC(); // Récupération dans un tableau des données de l'utilisateur lié à ses commandes
+            $array['dataC'] = $this->Model_espaceclient->getClient(); // Récupération dans un tableau des infos client
+            $navbarRub['getRubriques'] = $this->Model_home->getRubriques(); // Récupération dans un tableau des rubriques et sous rubriques pour la navbar
+            // Chargement des vues et envoie des différents tableaux nécessaires
+            $this->load->view('include/incl_head');
+            $this->load->view('include/incl_navbar', $navbarRub);
+            $this->load->view('incl_user_area', $array);
+            $this->load->view('include/incl_footer');
+            $this->load->view('include/incl_script');
         }
+    }
 
-public function detailsCommande($idcommande){
-
-    $array['dataC'] = $this->Model_espaceclient->getClient();
-    $array['dataCommande'] = $this->Model_espaceclient->getCommande($idcommande);
-    $data['getRubriques'] = $this->Model_home->getRubriques();
-    $this->load->view('include/incl_loader');
-    $this->load->view('include/incl_navbar', $data);
-    $this->load->view('include/incl_head');
-    $this->load->view('incl_detailscommande', $array);
-    $this->load->view('include/incl_footer');
-    $this->load->view('include/incl_script');
-}
-
+    public function detailsCommande($idcommande) // Fonction detailsCommande qui charge le détail de la commande passée
+    {
+        $array['dataC'] = $this->Model_espaceclient->getClient(); // Récupération dans un tableau des infos client
+        $array['dataCommande'] = $this->Model_espaceclient->getCommande($idcommande); // Récupération dans un tableau des infos de la commande du client
+        $data['getRubriques'] = $this->Model_home->getRubriques(); // Récupération dans un tableau des rubriques et sous rubriques pour la navbar
+        // Chargement des vues et envoie des différents tableaux nécessaires
+        $this->load->view('include/incl_loader');
+        $this->load->view('include/incl_navbar', $data);
+        $this->load->view('include/incl_head');
+        $this->load->view('incl_detailscommande', $array);
+        $this->load->view('include/incl_footer');
+        $this->load->view('include/incl_script');
+    }
 }
--- application/models/Model_espaceclient.php
+++ application/models/Model_espaceclient.php
@@ -2,58 +2,51 @@
 
 class Model_espaceclient extends CI_Model
 {
-    public function getClient()
-    {
+    public function getClient() // Fonction appelée dans la fonction espaceC du controller espaceclient
+    { // Fonction qui sert à récupérer les infos nécessaires sur le client
 
-        $user_id = $this->session->userdata('user_id');
+        $user_id = $this->session->userdata('user_id'); // Récupération de l'id client selon les données de session
 
-        if ($user_id > 0) {
-
+        if ($user_id > 0) { // Si l'id est correct
             $this->db->select('client_id, client_nom, client_prenom, client_adresse, client_ville, client_codepo, client_telephone, client_mail, client_type, client_siret, client_role_id, client_commerciaux_id');
-            $this->db->from('clients');
-            $this->db->where('client_id', $user_id);
-            $select = $this->db->get();
+            $this->db->from('clients'); // Sélection des champs nécessaires de la table clients
+            $this->db->where('client_id', $user_id);  // Selon l'id récupérée
+            $select = $this->db->get(); // Affectation du résultat à la variable $select
 
-            return $select->result();
+            return $select->result(); // Envoie du résultat en objet
         }
-         else {
+         else { // Si l'id est incorrect renvoie false
             return false;
         }
     }
 
+    public function getEspaceC() // Fonction appelée dans la fonction espaceC du controller espaceclient
+    { // Fonction qui relie les commandes potentiellement présentes avec l'id du client
 
-    public function getEspaceC()
-    {
+        $user_id = $this->session->userdata('user_id'); // Récupération de l'id du client selon l'id de session
 
-        $user_id = $this->session->userdata('user_id');
-
-        if ($user_id > 0) {
+        if ($user_id > 0) { // Si l'id est correct
 
             $this->db->select('*');
-            $this->db->from('clients');
-            $this->db->join('commande', 'clients.client_id = commande.commande_client_id');
-            $this->db->where('client_id', $user_id);
-            $select = $this->db->get();
+            $this->db->from('clients'); // Sélection de tous les champs de la table clients et commande
+            $this->db->join('commande', 'clients.client_id = commande.commande_client_id'); // Jointure selon l'id du client
+            $this->db->where('client_id', $user_id); // Où l'id du client est égal à celui récupéré
+            $select = $this->db->get(); // Affectation dur ésultat à la variable $select
 
-            return $select->result();
-        }
-         else {
+            return $select->result(); // Renvoie du résultat en objet
+        } else { // Si l'id est incorrect renvoie false
             return false;
         }
     }
 
-    public function getCommande($idcommande){
+    public function getCommande($idcommande){ // Fonction appelée dans la fonction detailsCommande du controller espaceclient
 
         $this->db->select('*');
-        $this->db->from('secomposede');
-        $this->db->join('commande', 'secomposede.secomposede_commande_id = commande.commande_id');
-        $this->db->where('commande_id', $idcommande);
-        $select = $this->db->get();
+        $this->db->from('secomposede'); // Sélection de tous les champs de la table commande et secomposede
+        $this->db->join('commande', 'secomposede.secomposede_commande_id = commande.commande_id'); // Jointure selon l'id de commande
+        $this->db->where('commande_id', $idcommande); // Où l'id de la commande est égal à celui passé en paramètre
+        $select = $this->db->get(); // Affectation du résultat à la variable $select
 
-        return $select->result();
-
+        return $select->result(); // Renvoie du résultat en objet
     }
 }
-
-
-?>
