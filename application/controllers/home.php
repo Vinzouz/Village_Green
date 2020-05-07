acf67f025fb43b532dad438a4c4deb370db9323a
unknown <vincent.andre1893@gmail.com>
2020-05-07 19:28:28 +0200

    Commit Vinz
    Commentaires contrôlleur et modèle home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,30 +3,33 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Home extends CI_Controller
 {
 
-	public function __construct(){
+	public function __construct(){ // Fonction constructrice qui charge les modèles pour tout le controller
 		parent::__construct();
 		$this->load->model('Model_home');
 	}
 
-	public function index()
+	public function index() // Fonction index qui charge la page d'accueil
 	{
-        $data['getRubriques'] = $this->Model_home->getRubriques();
-        $data['carrousel_produit'] = $this->Model_home->carrousel_produit();
+        $data['getRubriques'] = $this->Model_home->getRubriques(); // Récupération dans un tableau des rubriques liées au sous rubriques renvoyé par la fonction getRubriques du modèle home
+        $data['carrousel_produit'] = $this->Model_home->carrousel_produit(); // Récupération dans un tableau de produits aléatoires pour le carousel renvoyé par la fonction carrousel_produit du modèle home
 
+        // Chargement de la vue principale et envoie des tableaux pour l'affichage
 		$this->load->view('index/index', $data);
 	}
 
-    public function inscription()
+    public function inscription() // Fonction inscription qui charge le formulaire d'inscription
     {
+        // Simple chargement des vues nécessaires
     	$this->load->view( 'include/incl_loader' );
         $this->load->view( 'include/incl_head' );
         $this->load->view( 'login/incl_forminscription' );
         $this->load->view( 'include/incl_script' );
 	}
 	
-	public function apropos()
+	public function apropos() // Fonction apropos qui charge les différentes informations des créateurs du site
     {
-        $data['getRubriques'] = $this->Model_home->getRubriques();
+        $data['getRubriques'] = $this->Model_home->getRubriques(); // Récupération dans un tableau des rubriques liées au sous rubriques renvoyé par la fonction getRubriques du modèle home
+        // Chargement des vues et envoie du tableau pour les rubriques
         $this->load->view( 'include/incl_loader' );
         $this->load->view('include/incl_navbar', $data);
         $this->load->view('include/incl_head');
--- application/models/Model_home.php
+++ application/models/Model_home.php
@@ -2,34 +2,28 @@
 
 class Model_home extends CI_Model {
 
-    public function getRubriques(){
+    public function getRubriques(){ // Fonction appelée dans la fonction index du controller home
 
-
-        $parents =  $this->db->get('rubrique');
-
-        $parents = $parents->result_array();
+        $parents =  $this->db->get('rubrique'); // Sélection de la table des rubriques
+        $parents = $parents->result_array(); // Affectation du résultat en tableau
         
-        
-        foreach($parents as $key=> $parent){
+        foreach($parents as $key=> $parent){ // Boucle pour chaque résultat de rubrique
 
             //echo $parent->rubrique_id;
-            $this->db->where('sousrub_rubrique_id', $parent['rubrique_id']);
-            $child = $this->db->get('sous_rubrique');
-            $parents[$key]['child'] = $child->result_array();
- 
+            $this->db->where('sousrub_rubrique_id', $parent['rubrique_id']); // Sélection des sous rubriques qui ont l'idée de la rubrique
+            $child = $this->db->get('sous_rubrique'); // Affectation du résultat à la variable $child
+            $parents[$key]['child'] = $child->result_array(); // Affectation en tableaux pour chaque rubrique
         }
-        return $parents;
+        return $parents; // Renvoie le tableau en entier
     }
 
-    public function carrousel_produit(){
+    public function carrousel_produit(){ // Fonction appelée dans la fonction index du controller home
 
-        $this->db->select('*');
-        $this->db->from('sous_rubrique')->order_by('produit_id', 'RANDOM')->limit(12);
-        $this->db->join('produit', 'produit_sousrub_id = sousrub_id');
-        $selected = $this->db->get();
+        $this->db->select('*'); // Sélection de tous les champs
+        $this->db->from('sous_rubrique')->order_by('produit_id', 'RANDOM')->limit(12); // De la table sous-rubrique par ordre d'id qui choisit aléatoirement 12 résultats
+        $this->db->join('produit', 'produit_sousrub_id = sousrub_id'); // Jointure de la table produit par id de sous rubrique
+        $selected = $this->db->get(); // Affectation du resultat à la variable $selected
 
-        return $selected->result();
+        return $selected->result(); // Renvoie du résultat en objet
     }
-
-
 }
