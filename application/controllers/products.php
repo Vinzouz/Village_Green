23628e21b2b516bf1e7e28bacd38d091cd88eecf
unknown <vincent.andre1893@gmail.com>
2020-05-08 18:37:40 +0200

    Commit Vinz
    Commentaires contrôleur et modèle products

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -3,21 +3,19 @@
 class products extends CI_Controller
 {
 
-    public function __construct()
+    public function __construct() // Fonction constructrice qui charge les modèles pour tout le controller
     {
         parent::__construct();
         $this->load->model('Model_products');
         $this->load->model('Model_home');
     }
 
-
-    public function getRubrique($Rubrique_id)
+    public function getRubrique($Rubrique_id) // Fonction getRubrique qui sert à charger les cards des différentes sous-rubriques
     {
-
-        $data['getRubrique'] = $this->Model_products->getRubrique($Rubrique_id);
-        $array['getRubriques'] = $this->Model_home->getRubriques();
-        $prod['carrousel_produit'] = $this->Model_home->carrousel_produit();
-
+        $data['getRubrique'] = $this->Model_products->getRubrique($Rubrique_id); // Récupération dans un tableau des rubriques liées aux sous rubriques renvoyé par la fonction getRubrique du modèle products
+        $array['getRubriques'] = $this->Model_home->getRubriques(); // Récupération dans un tableau des rubriques liées au sous rubriques renvoyé par la fonction getRubriques du modèle home
+        $prod['carrousel_produit'] = $this->Model_home->carrousel_produit(); // Récupération dans un tableau de produits aléatoires pour le carousel renvoyé par la fonction carrousel_produit du modèle home
+        // Chargement des vues et envoie des tableaux aux différentes vues
         $this->load->view('include/incl_loader');
         $this->load->view('include/incl_navbar', $array);
         $this->load->view('include/incl_head');
@@ -28,14 +26,13 @@ class products extends CI_Controller
         $this->load->view('include/incl_script');
     }
 
-    public function getSousRubrique($SousRubrique_id)
+    public function getSousRubrique($SousRubrique_id) // Fonction getSousRubrique qui sert à charger les cards des produits selon la sous-rubrique
     {
-
-        $data['getRubriques'] = $this->Model_home->getRubriques();
-        $array['getRubriques'] = $this->Model_home->getRubriques();
-        $data['getSousRubrique'] = $this->Model_products->getSousRubrique($SousRubrique_id);
-        $prod['carrousel_produit'] = $this->Model_home->carrousel_produit();
-
+        $data['getRubriques'] = $this->Model_home->getRubriques(); // Récupération dans un tableau des rubriques liées au sous rubriques renvoyé par la fonction getRubriques du modèle home
+        $array['getRubriques'] = $this->Model_home->getRubriques(); // Récupération dans un autre tableau des rubriques liées au sous rubriques renvoyé par la fonction getRubriques du modèle home
+        $data['getSousRubrique'] = $this->Model_products->getSousRubrique($SousRubrique_id); // Récupération dans un tableau des sous rubriques liées aux produits renvoyé par la fonction getRubrique du modèle products
+        $prod['carrousel_produit'] = $this->Model_home->carrousel_produit(); // Récupération dans un tableau de produits aléatoires pour le carousel renvoyé par la fonction carrousel_produit du modèle home
+        // Chargement des vues et envoie des tableaux aux différentes vues
         $this->load->view('include/incl_loader');
         $this->load->view('include/incl_navbar' , $array);
         $this->load->view('include/incl_head');
--- application/models/Model_products.php
+++ application/models/Model_products.php
@@ -2,21 +2,20 @@
 
 class Model_products extends CI_Model{
 
-    public function getRubrique($Rubrique_id){
-
-        $this->db->where('Rubrique_id', $Rubrique_id);
-        $this->db->join('sous_rubrique', 'sousrub_rubrique_id = rubrique_id');
-        $select = $this->db->get('Rubrique');
-        return $select->result();
+    public function getRubrique($Rubrique_id) // Fonction appelée par la fonction getRubrique du controller products
+    { // Fonction qui sert à lier les rubriques aux sous-rubriques
+        $this->db->where('Rubrique_id', $Rubrique_id); // Sélection de la rubrique par son id
+        $this->db->join('sous_rubrique', 'sousrub_rubrique_id = rubrique_id'); // Jointure avec la table sous-rubrique par l'id de rubrique
+        $select = $this->db->get('Rubrique'); // Affectation du résultat à la variable $select
+        return $select->result(); // Envoie du résultat en objet
     }
 
-    public function getSousRubrique($SousRubrique_id){
-
-        $this->db->where('Sousrub_id', $SousRubrique_id);
-        $this->db->join('produit', 'produit_sousrub_id = sousrub_id');
-        $this->db->join('rubrique', 'rubrique_id = sousrub_rubrique_id');
-        $selected = $this->db->get('sous_rubrique');
-
-        return $selected->result();
+    public function getSousRubrique($SousRubrique_id) // Fonction appelée par la fonction getSousRubrique du controller products
+    { // Fonction qui sert à lier les sous-rubriques aux produits
+        $this->db->where('Sousrub_id', $SousRubrique_id); // Sélection des lignes selon l'id de sous rubrique
+        $this->db->join('produit', 'produit_sousrub_id = sousrub_id'); // Jointure avec la table des produits
+        $this->db->join('rubrique', 'rubrique_id = sousrub_rubrique_id'); // Jointure avec la table des rubriques
+        $selected = $this->db->get('sous_rubrique'); // Affectation du résultat à la variable $selected
+        return $selected->result(); // Envoie du résultat en objet
     }
 }  
