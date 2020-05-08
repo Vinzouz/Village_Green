c1b4982412dd973b51c193a72ae89ea29ac82cb8
unknown <vincent.andre1893@gmail.com>
2020-05-08 18:27:47 +0200

    Commit Vinz
    Commentaires contrôleur et modèle search

--- application/models/Model_search.php
+++ application/models/Model_search.php
@@ -2,17 +2,12 @@
 
 class Model_search extends CI_Model{
 
-
-    public function get_search(){
-
-        $recherche = $this->input->post('search_value');
-        //$this->db->select('produit_nom');
-        $this->db->like('produit_nom', $recherche)->limit(10);
-        $this->db->or_like('produit_marque', $recherche);
-        
-        $select = $this->db->get('produit');
-
-        return $select->result_array();
-        
+    public function get_search() // Fonction appelée par la fonction get_search du controller search
+    {
+        $recherche = $this->input->post('search_value'); // Récupération du texte recherché
+        $this->db->like('produit_nom', $recherche)->limit(10); // Sélection du nom produit selon la recherche, limité à 10 résultats
+        $this->db->or_like('produit_marque', $recherche); // Ou sélection par la marque du produit
+        $select = $this->db->get('produit'); // Affectation des résultats à la variable $select
+        return $select->result_array(); // Envoie des résultats en tableau
     }
 }
